16a9f32d8781bcd3caf10bb3e3e654d506857c2f
SyedMahatabUddin <syedmahatabuddin930@gmail.com>
2026-06-02 19:37:47 +0600

    fixed cart carring cage img

--- src/Compoenet/Trial/Trial.jsx
+++ src/Compoenet/Trial/Trial.jsx
@@ -12,79 +12,79 @@ return (
      <div className="grid grid-cols-1 md:grid-cols-3 items-stretch justify-center mx-auto gap-5  max-w-300 mt-4">
 
 
-         <div class="w-full border-[#F6F6F6] bg-white flex flex-col justify-between my-10 pt-10 pb-20 px-5  rounded-2xl border-2 ">
-           <div class="$$card-body space-y-3">
+         <div className="w-full border-[#F6F6F6] bg-white flex flex-col justify-between my-10 pt-10 pb-20 px-5  rounded-2xl border-2 ">
+           <div className="$$card-body space-y-3">
            <div className="">
-             <span class="text-2xl font-bold text-[#101727] ">Most Popular</span>
+             <span className="text-2xl font-bold text-[#101727] ">Most Popular</span>
              <p className="text-[16px] text-[#627382] ">Best for professionals</p>
            </div>
-               <p class="text-[40px] font-bold">$29<span class="text-xl text-[#627382] ">/Month</span></p>
-             <ul class="mt-6 flex flex-col gap-2 text-[16px]">
+               <p className="text-[40px] font-bold">$29<span className="text-xl text-[#627382] ">/Month</span></p>
+             <ul className="mt-6 flex flex-col gap-2 text-[16px]">
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span>High-resolution image generation</span>
                </li>
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span>Customizable style templates</span>
                </li>
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span>Batch processing capabilities</span>
                </li>
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span>AI-driven image enhancements</span>
                </li>
 
                
              </ul>
-             <div class="mt-6">
-             <button class="btnPrimary  w-full font-light  ">Subscribe</button>
+             <div className="mt-6">
+             <button className="btnPrimary  w-full font-light  ">Subscribe</button>
              </div>
            </div>
          </div>
 
 
-         <div class=" border-[#F6F6F6] flex flex-col items-center my-10 pt-10 pb-20 px-5  rounded-2xl border-2 space-y-4 bg-linear-to-r  from-[#4F39F6] to-[#9514FA] relative">
-           <div class="$$card-body space-y-3">
+         <div className=" border-[#F6F6F6] flex flex-col items-center my-10 pt-10 pb-20 px-5  rounded-2xl border-2 space-y-4 bg-linear-to-r  from-[#4F39F6] to-[#9514FA] relative">
+           <div className="$$card-body space-y-3">
             <div className="bg-[#FEF3C6] py-2 px-4 rounded-full mx-auto w-fit justify-items-center absolute -top-4 left-1/2 -translate-x-1/2 shadow-sm whitespace-nowrap">
               <p className="text-[#BB4D00] font-medium">Most Popular</p>
             </div>
            <div className="">
-             <span class="text-2xl font-bold text-white">Most Popular</span>
+             <span className="text-2xl font-bold text-white">Most Popular</span>
              <p className="text-[16px] text-white ">Best for professionals</p>
            </div>
-               <p class="text-[40px] font-bold text-white ">$29<span class="text-xl text-white font-extralight ">/Month</span></p>
-             <ul class="mt-6 flex flex-col  gap-2 text-[16px] ">
+               <p className="text-[40px] font-bold text-white ">$29<span className="text-xl text-white font-extralight ">/Month</span></p>
+             <ul className="mt-6 flex flex-col  gap-2 text-[16px] ">
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span className="text-white">High-resolution image generation</span>
                </li>
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span className="text-white">Customizable style templates</span>
                </li>
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span className="text-white">Batch processing capabilities</span>
                </li>
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span className="text-white">AI-driven image enhancements</span>
                </li>
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span className="text-white">Batch processing capabilities</span>
                </li>
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span className="text-white">AI-driven image enhancements</span>
                </li>
 
              </ul>
-             <div class="mt-6 rounded-2xl bg-white hover:bg-gray-100 hover:-translate-y-0.5 hover:scale-103 transition-transform ease-in-out duration-200">
-             <button class="flex justify-center border  items-center bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent font-bold text-[16px] cursor-pointer py-1 px-3.5 w-full ">Subscribe</button>
+             <div className="mt-6 rounded-2xl bg-white hover:bg-gray-100 hover:-translate-y-0.5 hover:scale-103 transition-transform ease-in-out duration-200">
+             <button className="flex justify-center border  items-center bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent font-bold text-[16px] cursor-pointer py-1 px-3.5 w-full ">Subscribe</button>
              </div>
            </div>
          </div>
@@ -92,42 +92,42 @@ return (
 
 
 
-         <div class=" border-[#F6F6F6] bg-white flex flex-col items-center my-10 pt-10 pb-20 px-5  rounded-2xl border-2 space-y-4 hover">
-           <div class="$$card-body space-y-3">
+         <div className=" border-[#F6F6F6] bg-white flex flex-col items-center my-10 pt-10 pb-20 px-5  rounded-2xl border-2 space-y-4 hover">
+           <div className="$$card-body space-y-3">
            <div className="">
-             <span class="text-2xl font-bold text-[#101727] ">Most Popular</span>
+             <span className="text-2xl font-bold text-[#101727] ">Most Popular</span>
              <p className="text-[16px] text-[#627382] ">Best for professionals</p>
            </div>
-               <p class="text-[40px] font-bold">$29<span class="text-xl text-[#627382] ">/Month</span></p>
-             <ul class="mt-6 flex flex-col gap-2 text-[16px] ">
+               <p className="text-[40px] font-bold">$29<span className="text-xl text-[#627382] ">/Month</span></p>
+             <ul className="mt-6 flex flex-col gap-2 text-[16px] ">Name
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span>High-resolution image generation</span>
                </li>
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span>Customizable style templates</span>
                </li>
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span>Batch processing capabilities</span>
                </li>
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span>AI-driven image enhancements</span>
                </li>
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span>AI-driven image enhancements</span>
                </li>
                <li>
-                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
                  <span>AI-driven image enhancements</span>
                </li>
 
              </ul>
-             <div class="mt-6">
-             <button class="btnPrimary  w-full font-light ">Subscribe</button>
+             <div className="mt-6">
+             <button className="btnPrimary  w-full font-light ">Subscribe</button>
              </div>
            </div>
          </div>
--- src/Compoenet/Workflow/Workflow.jsx
+++ src/Compoenet/Workflow/Workflow.jsx
@@ -5,8 +5,8 @@ return (
      <h5 className="text-[40px] font-extrabold text-[#FFFFFF] ">Ready to Transform Your Workflow?</h5>
      <p className="text[16px] text-white ">Join thousands of professionals who are already using Digitools to work smarter. <br></br> Start your free trial today.</p>
      <div className='flex gap-4 py-6 justify-center'>
-       <div class="rounded-full bg-white hover:bg-gray-100 hover:-translate-y-0.5 hover:scale-103 transition-transform ease-in-out duration-200">
-             <button class=" items-center bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent font-bold text-[16px] cursor-pointer py-2 px-5 ">Subscribe</button>
+       <div className="rounded-full bg-white hover:bg-gray-100 hover:-translate-y-0.5 hover:scale-103 transition-transform ease-in-out duration-200">
+             <button className=" items-center bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent font-bold text-[16px] cursor-pointer py-2 px-5 ">Subscribe</button>
        </div>
       
        <button className='flex rounded-full border border-white hover:bg-purple-700 hover:-translate-y-0.5 hover:scale-103 transition-transform ease-in-out duration-200 py-2 px-5  items-center font-semibold text-white text-[16px] '> Watch Demo</button>
